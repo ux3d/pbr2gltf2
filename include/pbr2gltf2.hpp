651b61e7b310745e883249354a34b31c19715caa
Moritz Willy Becher <becher@ux3d.io>
2021-11-18 12:08:32 +0100

    rename function to convert to filesystem

--- include/pbr2gltf2.hpp
+++ include/pbr2gltf2.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace pbr2gltf2 {
-int convert(const std::string& path, float defaultMetallicFactor, float defaultRoughnessFactor, bool keepNormalImageData, bool keepEmissiveImageData);
+int convert_to_filesystem(const std::string& path, float defaultMetallicFactor, float defaultRoughnessFactor, bool keepNormalImageData, bool keepEmissiveImageData);
 }
 
 #endif // PBR2GLTF2_H
